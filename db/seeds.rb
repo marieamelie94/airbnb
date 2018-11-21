9fbef2c413a1ed27b526c6963b0b8dace2a849f8
Roger Comas <roger97ct@gmail.com>
2018-11-21 15:33:58 +0100

    flats added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: 'Dodo', email: 'dodo@nobrain.com')
+puts "creating user"
+user = User.create!(
+  name: 'Host1',
+  email: 'hodooeee@nobrain.com',
+  password: "blahah"
+  )
+user_two = User.create!(name: 'Dodo', email: 'jodoooee@nobrain.com', password: "blahah")
 
-Flat.create(name: 'A flat', description: 'blblabalaaaaa', address: 'checkpoint charlie, berlin', user: user1 )
+puts "creating flats"
+Flat.create!(name: 'Romantic shithole',
+ user_id: user.id,
+ description: 'Full package creep exprecience. You will never feel the same again, Spend time with our Host, he will take a breath away.',
+ photo: 'https://res.cloudinary.com/mas-cloud/image/upload/v1542797858/Flats/flat1.jpg',
+ price: 300,
+ wifi: true,
+ kitchen: true,
+ air_conditionning: false,
+ category: 'super trash',
+ address: ' Schönwalder Allee 26, 13587 Berlin',
+ flat_type: 'room')
+# Flat.create!(name: 'Reepy', user_id: user_two.id)
