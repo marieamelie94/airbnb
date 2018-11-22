6d080664daaf1ba6ace81fdf1e70b38fd346e987
Marie Amelie Sandrock <msandrock@audencia.com>
2018-11-22 14:52:04 +0100

    booking creation works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def create
+    # @booking = current_user.booking.create(booking_params)
+    @booking = Booking.new(booking_params)
+    @flat = Flat.find(params[:flat_id])
+    @user = current_user
+    @booking.user = @user
+    @booking.flat = @flat
+    if @booking.save
+      redirect_to '/pages/checkout'
+    else
+      render 'flat_path(@flat)'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flat_id, :user_id, :check_in, :checkout)
+  end
 end
