9a326c9d35ef53b7d5a2f09dda39d8ba09640ee5
Marie Amelie Sandrock <msandrock@audencia.com>
2018-11-22 18:13:26 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+User.destroy_all
 
 puts "creating user"
 user = User.create!(
